e8a938992150273d825aa19dd95febf2e24a5398
sermain2 <87350415+sermain2@users.noreply.github.com>
2023-02-15 23:45:37 +0900

    Add files via upload
    거스름돈을 계산해 주는 프로그램

---
+++ defcalc.py
@@ -0,0 +1,16 @@
+def calculate_change(payment, cost):
+    change = payment - cost
+    
+    fifty_count = change // 50000
+    ten_count = (change % 50000) // 10000
+    five_count = (change % 10000) // 5000
+    one_count = (change % 5000) // 1000
+    
+    print("50000원 지폐: {}장".format(fifty_count))
+    print("10000원 지폐: {}장".format(ten_count))
+    print("5000원 지폐: {}장".format(five_count))
+    print("1000원 지폐: {}장".format(one_count))
+
+calculate_change(100000, 33000)
+print()
+calculate_change(500000, 378000)
